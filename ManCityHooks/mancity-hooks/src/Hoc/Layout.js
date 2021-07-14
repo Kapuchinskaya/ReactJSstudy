22ee0e1792b907f03bbab0b297ca61464615657e
olga <global>
2021-07-14 14:54:32 +0200

    feat: add header

--- ManCityHooks/mancity-hooks/src/Hoc/Layout.js
+++ ManCityHooks/mancity-hooks/src/Hoc/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Header from "../Components/Header_footer/Header";
 
 const Layout = (props) => {
   return (
     <div>
-                  {/* <Header/> */}
-                  {props.children}
-                  {/* <Footer/> */}
+                   
+      <Header />             
+      {props.children}
+      {/* <Footer/> */}
               
     </div>
   );
 };
 
-export default Layout;
+export default Layout;
