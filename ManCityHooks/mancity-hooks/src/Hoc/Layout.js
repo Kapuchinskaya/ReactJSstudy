4ac5aedfe5f0e9a87febf28207d1c9666bf512fa
olga <global>
2021-07-14 13:49:42 +0200

    feat: add hoc (Layout.js) for header and footer

---
+++ ManCityHooks/mancity-hooks/src/Hoc/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Layout = (props) => {
+  return (
+    <div>
+                  {/* <Header/> */}
+                  {props.children}
+                  {/* <Footer/> */}
+              
+    </div>
+  );
+};
+
+export default Layout;
--- ManCityHooks/mancity-hooks/src/routes.js
+++ ManCityHooks/mancity-hooks/src/routes.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "./Hoc/Layout";
 
 const Routes = (props) => {
-  return <div>hello</div>;
+  return <Layout>hello</Layout>;
 };
 
 export default Routes;
