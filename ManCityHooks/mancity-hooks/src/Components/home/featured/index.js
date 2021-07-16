013fa302216cb4918093b2d3dbcb24b8f366c4ea
olga <global>
2021-07-16 16:22:53 +0200

    feat: add stripes

--- ManCityHooks/mancity-hooks/src/Components/home/featured/index.js
+++ ManCityHooks/mancity-hooks/src/Components/home/featured/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Stripes from './Stripes';
-import Text from './Text';
+// import Text from './Text';
 
 const Featured = () => {
     return (
         <div className="featured_wrapper">
             <Stripes/>
             {/* <Text/> */}
-            some text
+            {/* some text */}
         </div>
     );
 };
