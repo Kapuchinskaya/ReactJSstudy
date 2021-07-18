e25fe6dee2b8cdf53ca9ccab24d8be4b190182f1
olga <global>
2021-07-18 16:23:10 +0200

    feat: js to tsx (ending)

--- ManCityHooks/mancity-hooks/src/Components/Common/types.ts
+++ ManCityHooks/mancity-hooks/src/Components/Common/types.ts
@@ -1,3 +1,3 @@
 export type ArrowFunctionElement = () => JSX.Element;
-
+export type ArrowFunctionElementWithProps = (props: any) => JSX.Element;
 export type ArrowFunctionElements = () => JSX.Element[];
--- ManCityHooks/mancity-hooks/src/routes.tsx
+++ ManCityHooks/mancity-hooks/src/routes.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Layout from "./Hoc/Layout";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./Components/home";
 
-const Routes = (props) => {
+const Routes = () => {
   return (
     <Layout>
       <Switch>
